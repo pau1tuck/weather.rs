c8c227f2aca3c18b4eb928ec5e6ac211b3beabc9
Paul Tuck <paul.tuck@oxon.org>
2023-02-02 17:06:23 +0700

    "2023-02-02_17:06_ICT+7"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let weather_data = reqwest::Client::new()
+        .get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mae%20Chan?unitGroup=metric&include=current&key=ASRQL55QSXYKL63Y2DCEMFUC3&contentType=json")
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{:#?}", weather_data);
+
+    Ok(())
 }
